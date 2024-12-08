ebb6bc0ec598e7dee9b7b93f4a9ae592ff247e4a
denskrlv <d.krylov@student.utwente.nl>
2024-12-08 15:02:29 +0100

    Refactored FeatureAnalyzer into a separate class.

--- src/team_1/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def load_data_from(file_path):
-    curr_dir = os.getcwd()
-    project_dir = os.path.dirname(os.path.dirname(curr_dir))
-    data_dir = os.path.join(project_dir, file_path)
-    return data_dir
